1ce1527fb1fad18c564160c81a5b847d2b85379b
Mishita Jain <jainmishita1977@gmail.com>
2025-08-25 17:01:54 +0530

    frontend modified acc to deployed backend url

--- my_portfolio/src/components/Contact.jsx
+++ my_portfolio/src/components/Contact.jsx
@@ -25,16 +25,14 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch(
-      "https://mjportfolio-efy0.onrender.com/contact",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify(formDetails),
-      }
-    );
+    let response = await fetch(`${import.meta.env.VITE_API_BASE}/contact`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
